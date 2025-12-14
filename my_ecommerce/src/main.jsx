0d591925c7a963d62ace5be463114400aee7a8c2
PratickBaraik <animeNerd.int@protonmail.com>
2025-12-14 19:31:05 +0530

    deleting some files and adding content to main.jsx page

--- my_ecommerce/src/main.jsx
+++ my_ecommerce/src/main.jsx
@@ -1,13 +1,16 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../src/static/pages/home.jsx'
+import About from '../src/static/pages/about.jsx'
+import Contact from '../src/static/pages/contact.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element='' />
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/contact' element={<Contact />}/>
     </Routes>
   </BrowserRouter>
 )
